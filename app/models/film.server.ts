9cc9735589a7f30cf8fad06007e6b41e6f84fac5
cfbonner <cfbonner@gmail.com>
2023-02-26 17:00:32 +0000

    Add resource dashboard

---
+++ app/models/film.server.ts
@@ -0,0 +1,41 @@
+import type { Film, User } from "@prisma/client";
+import { prisma } from "~/db.server";
+
+export function getFilms() {
+  return prisma.film.findMany();
+}
+
+export function createFilm({
+  title,
+  synopsis,
+  userId,
+}: Pick<Film, "title" | "synopsis"> & {
+  userId: User["id"];
+}) {
+  return prisma.film.create({
+    data: {
+      title,
+      synopsis,
+      user: {
+        connect: {
+          id: userId,
+        },
+      },
+    },
+  });
+}
+
+export function deleteFilm({
+  id,
+  userId,
+}: { id: Film["id"] } & {
+  userId: User["id"];
+}) {
+  console.log({ id, userId });
+  return prisma.film.deleteMany({
+    where: {
+      id: id,
+      userId: userId,
+    },
+  });
+}
---
+++ app/routes/films.tsx
@@ -0,0 +1,63 @@
+import { Form, Link, NavLink, Outlet, useLoaderData } from "@remix-run/react";
+import { json, LoaderArgs } from "@remix-run/server-runtime";
+import { getFilms } from "~/models/film.server";
+import { requireUser } from "~/session.server";
+import { useUser } from "~/utils";
+
+export async function loader({ request }: LoaderArgs) {
+  await requireUser(request);
+  const filmListItems = await getFilms();
+  return json({ filmListItems });
+}
+
+export default function FilmsPage() {
+  const user = useUser();
+  const data = useLoaderData<typeof loader>();
+
+  return (
+    <div className="flex h-full min-h-screen flex-col">
+      <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
+        <h1 className="text-3xl font-bold">
+          <Link to=".">Films</Link>
+        </h1>
+        <p>{user.email}</p>
+        <Form action="/logout" method="post">
+          <button
+            type="submit"
+            className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+          >
+            Logout
+          </button>
+        </Form>
+      </header>
+
+      <main className="flex h-full bg-white">
+        <div className="h-full w-80 border-r bg-gray-50">
+          {data.filmListItems.length === 0 ? null : (
+            <ol>
+              {data.filmListItems.map((film) => (
+                <li key={film.id}>
+                  <NavLink
+                    className={({ isActive }) =>
+                      `block border-b p-4 text-xl ${isActive ? "bg-white" : ""}`
+                    }
+                    to={film.id}
+                  >
+                    {film.title}
+                  </NavLink>
+                </li>
+              ))}
+            </ol>
+          )}
+          <Link to="new" className="block p-4 text-xl text-blue-500">
+            + New Film
+          </Link>
+        </div>
+
+        <div className="flex-1 p-6">
+          <Outlet />
+        </div>
+      </main>
+    </div>
+  );
+}
